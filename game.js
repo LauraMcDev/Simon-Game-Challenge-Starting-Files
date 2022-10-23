9d75865a1a28db22ff2c37497a1998cf4c7514c2
LauraMcDev <lauramcdev089@gmail.com>
2022-10-23 16:01:05 +0200

    working sounds and animations on click

--- game.js
+++ game.js
@@ -2,8 +2,19 @@ let userClickedPattern = []
 
 let gamePattern = [];
 
-
 let buttonColors = ["red", "blue", "green", "yellow"];
+
+
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColor);
+    // console.log(userClickedPattern);
+    playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+});
+
 
 
 function nextSequence () {
@@ -12,15 +23,21 @@ function nextSequence () {
     gamePattern.push(randomChosenColor);
     
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    let sound = new Audio("sounds/" + randomChosenColor + ".mp3");
-    sound.play();
+    playSound(randomChosenColor);
 }
 
 
-$(".btn").click(function() {
-    let userChosenColor = $(this).attr("id");
 
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
-});
+
+function playSound(name) {
+   let sound = new Audio("sounds/" + name + ".mp3");
+    sound.play();  
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+
+}
