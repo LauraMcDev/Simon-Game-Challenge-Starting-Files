4440754e94a92def64cb1c33aec9ab4b7b64e4b1
LauraMcDev <lauramcdev089@gmail.com>
2022-11-02 22:32:28 +0100

    JS completed, tested and working

--- game.js
+++ game.js
@@ -1,9 +1,21 @@
-let userClickedPattern = []
+let userClickedPattern = [];
 
 let gamePattern = [];
 
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+let level = 0;
+
+let started = false;
+
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+
+    }
+});
 
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id");
@@ -11,22 +23,54 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColor);
     // console.log(userClickedPattern);
     playSound(userChosenColor);
-
     animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
 });
 
 
 
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("h1").text("Game Over, Press any Key to Restart");
+        
+        startOver();
+    }
+
+}
+
 function nextSequence () {
+
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
+
 }
-
-
 
 
 function playSound(name) {
@@ -40,4 +84,10 @@ function animatePress(currentColor) {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
